31ccaf36dcfb5c7a7d75a51d3a9c939856a2fbdc
LindaCr <lindacrook.44@gmail.com>
2021-08-27 23:13:01 -0400

    init commit

---
+++ forloop-practice/src/ForLoopPractice.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class ForLoopPractice {
+
+	public static void main(String[] args) {
+		System.out.println("Hola");
+		System.out.println();
+		Scanner sc= new Scanner(System.in);
+		
+		String choice="y";
+		while (choice.equalsIgnoreCase("y")) {
+			
+			System.out.println("Enter a number 1-10");
+			int n=sc.nextInt();
+			
+			printArrow(n);
+			
+			System.out.println("Continue? y/n"); 
+			choice=sc.next();
+		}
+		
+		sc.close();
+		System.out.println("Peace");
+
+	}
+
+	static void printArrow(int n)
+	{
+
+	    for (int i = 1; i < n; i++) {
+
+	        for (int j = 0; j < i; j++) {
+	            System.out.print(" ");
+	        }
+
+	        for (int k = 0; k < i; k++) {
+	            System.out.print("*");
+	        }
+	        System.out.println();
+	    }
+
+	    for (int i = 0; i < n; i++) {
+
+	        for (int j = 0; j < n - i; j++) {
+	            System.out.print(" ");
+	        }
+
+	        for (int k = 0; k < n - i; k++) {
+	            System.out.print("*");
+	        }
+	        System.out.println();
+	    }
+	}
+}
