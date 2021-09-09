b8030f521dc2c6028fe2f18b2cbea7a317880433
LindaCr <lindacrook.44@gmail.com>
2021-09-08 22:33:01 -0400

    init commit

---
+++ reading-input-practice/src/RegistrationApp.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class RegistrationApp {
+
+	public static void main(String[] args) {
+		System.out.println("Student Registration Form");
+		
+		Scanner sc= new Scanner(System.in);
+		System.out.println();
+	
+		System.out.print("Enter first name: ");
+		String firstName= sc.next();
+		System.out.println(firstName);
+		
+		System.out.println("Enter Last Name: ");
+		String lastName= sc.next();
+		System.out.println(lastName);
+		
+		System.out.println("Enter Year of Birth: ");
+		int birthYear= sc.nextInt();
+		System.out.println(birthYear);
+		
+		
+		System.out.println("Welcome " + firstName+" "+lastName+ "!");
+		System.out.println("Your registration is complete.");
+		System.out.println("Your temporary password is: " +firstName+"*"+birthYear);
+		
+		sc.close();
+		System.out.println("Bye");
+
+	}
+
+}
